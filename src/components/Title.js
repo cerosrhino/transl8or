a47b58c1be4dd7c4ce4584881497058e34360931
unknown <cerosrhino@users.noreply.github.com>
2018-05-05 03:18:23 +0200

    Fixed non-breaking space causing misaligned text

--- src/components/Title.js
+++ src/components/Title.js
@@ -5,14 +5,14 @@ import './Title.css';
 class Title extends Component {
   render() {
     return (
-      <p className="title">
+      <div className="title">
         { this.props.text }
-        &nbsp;
+        { ' ' }
         {
           this.props.onEncodingChange &&
           <EncodingPicker onChange={this.props.onEncodingChange}/>
         }
-      </p>
+      </div>
     );
   }
 }
